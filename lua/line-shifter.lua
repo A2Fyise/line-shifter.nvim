cd50c3f2e4b06b89243a0c0571ff540eaf2c8d0a
aseeralfaisal <aseeralfaisal@gmail.com>
2024-12-10 01:00:26 +0600

    cut option added

--- lua/line-shifter.lua
+++ lua/line-shifter.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup()
   vim.keymap.set('n', '<C-j>', '<CMD>:m+1<CR>', { noremap = true, silent = true })
   vim.keymap.set('n', '<C-k>', '<CMD>:m-2<CR>', { noremap = true, silent = true })
-  vim.keymap.set('n', '<C-x>', '<CMD>dd<CR>', { noremap = true, silent = true })
+  vim.keymap.set('n', '<C-x>', 'dd', { noremap = true, silent = true })
 end
 
 return M
