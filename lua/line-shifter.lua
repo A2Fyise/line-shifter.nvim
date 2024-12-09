89c871060a096c05fc74f198ea9aea439ab65027
aseeralfaisal <aseeralfaisal@gmail.com>
2024-12-10 02:26:03 +0600

    refactor

--- lua/line-shifter.lua
+++ lua/line-shifter.lua
@@ -1,9 +1,11 @@
 local M = {}
 
+local key = vim.keymap
+
 function M.setup()
-  vim.keymap.set('n', '<C-j>', '<CMD>:m+1<CR>', { noremap = true, silent = true })
-  vim.keymap.set('n', '<C-k>', '<CMD>:m-2<CR>', { noremap = true, silent = true })
-  vim.keymap.set('n', '<C-x>', 'dd', { noremap = true, silent = true })
+  key.set('n', '<C-j>', '<CMD>:m+1<CR>', { noremap = true, silent = true })
+  key.set('n', '<C-k>', '<CMD>:m-2<CR>', { noremap = true, silent = true })
+  key.set('n', '<C-x>', 'dd', { noremap = true, silent = true })
 end
 
 return M
