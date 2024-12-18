104d34a94c91193e10e1e31855084036511a3357
Aseer Al Faisal <67814164+A2Fyise@users.noreply.github.com>
2024-12-18 15:31:02 +0600

    Enhance keybindings for line manipulation and navigation
    - Add move line up/down with <C-j>/<C-k>
    - Implement delete line with <C-x>
    - Introduce yank line with <C-y>
    - Add paste above/below with <C-p>/<C-S-p>
    - Include indentation adjustments with <Tab>/<S-Tab>
    - Add quick save/quit with <C-s>/<C-q>
    - Implement fold toggling with <C-space>

--- lua/line-shifter.lua
+++ lua/line-shifter.lua
@@ -3,9 +3,18 @@ local M = {}
 local key = vim.keymap
 
 function M.setup()
-  key.set('n', '<C-j>', '<CMD>:m+1<CR>', { noremap = true, silent = true })
-  key.set('n', '<C-k>', '<CMD>:m-2<CR>', { noremap = true, silent = true })
-  key.set('n', '<C-x>', 'dd', { noremap = true, silent = true })
+  key.set('n', '<C-j>', '<CMD>m .+1<CR>==', { noremap = true, silent = true, desc = "Move line down" })
+  key.set('n', '<C-k>', '<CMD>m .-2<CR>==', { noremap = true, silent = true, desc = "Move line up" })
+  key.set('n', '<C-x>', 'dd', { noremap = true, silent = true, desc = "Delete line" })
+  key.set('n', '<C-y>', 'yy', { noremap = true, silent = true, desc = "Yank line" })
+  key.set('n', '<C-p>', 'P', { noremap = true, silent = true, desc = "Paste above" })
+  key.set('n', '<C-S-p>', 'p', { noremap = true, silent = true, desc = "Paste below" })
+  key.set('n', '<Tab>', '>', { noremap = true, silent = true, desc = "Indent line right" })
+  key.set('n', '<S-Tab>', '<', { noremap = true, silent = true, desc = "Indent line left" })
+  key.set('n', '<C-s>', '<CMD>w<CR>', { noremap = true, silent = true, desc = "Quick Save file" })
+  key.set('n', '<C-q>', '<CMD>q<CR>', { noremap = true, silent = true, desc = "Quit editor" })
+  key.set('n', '<C-q>', '<CMD>q<CR>', { noremap = true, silent = true, desc = "Quit & save file" })
+  key.set('n', '<C-space>', 'za', { noremap = true, silent = true, desc = "unfold" })
 end
 
 return M
